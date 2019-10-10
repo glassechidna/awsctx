27dcfdbfec248d1b293615c80057ebf1b7598050
Aidan Steele <aidan.steele@glassechidna.com.au>
2019-10-11 08:53:06 +1100

    bump

---
+++ bump.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ux
+
+git diff
+
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git commit -m "Update to match upstream" -a || true
+
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git push "${remote_repo}" HEAD:${INPUT_BRANCH}
